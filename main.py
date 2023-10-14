17013600985284da1d6dc15a9055f6febae64581
DuchInavsor <132691092+DuchInavsor@users.noreply.github.com>
2023-10-14 15:03:48 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import colorama
+from colorama import Fore
+import time
+
+print(Fore.MAGENTA + 'Break3rs.sec!' + Fore.RESET)
+
+time.sleep(3)
+
+print(Fore.RED + '[+]' + Fore.RESET + Fore.GREEN + 
+'Iniciando o sistema dos Break3rs...' + Fore.RESET)
+
+time.sleep(4)
+
+print(Fore.BLACK + 'Fase teste Break3rs.sec' + Fore.RESET)
